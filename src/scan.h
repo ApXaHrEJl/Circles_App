bf1173f475de04e97cfefe03e6011d845776f03b
Danya <danil.sitnikov2000@yandex.ru>
2020-04-27 21:03:04 +0700

    Fix scan.h for clang-format observation

--- src/scan.h
+++ src/scan.h
@@ -4,15 +4,15 @@
 
 typedef struct {
     char name[7];
-    int x,y;
+    int x, y;
     double r;
 } figure;
 
 void skip_char(size_t* i, char* line, size_t symb);
 
-int check_line(char *line, int *number, figure *circle);
+int check_line(char* line, int* number, figure* circle);
 
-size_t parse_coord(size_t *i, int *coord, char *line);
+size_t parse_coord(size_t* i, int* coord, char* line);
 
-size_t scan(char *str, figure *circle);
+size_t scan(char* str, figure* circle);
 #endif
