1649a53af5ad2c49f059a3f15740d34c00c0b0cd
Danya <danil.sitnikov2000@yandex.ru>
2020-04-27 19:02:30 +0700

    Add define guard

--- src/scan.h
+++ src/scan.h
@@ -1,3 +1,5 @@
+#ifndef H_SCAN
+#define H_SCAN
 #include <stddef.h>
 
 typedef struct {
@@ -13,3 +15,4 @@ int check_line(char *line, int *number, figure *circle);
 size_t parse_coord(size_t *i, int *coord, char *line);
 
 size_t scan(char *str, figure *circle);
+#endif
