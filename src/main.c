0e3d2b3d1467830c0e2a0cd45f7f30ba27e6c24f
Danya <danil.sitnikov2000@yandex.ru>
2020-05-06 12:07:55 +0700

    Fix crossing check

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@ int main(int argc, char** argv)
                circle[i].r);
         printf("\tperimeter = %.3lf\n", perimeter(circle, i));
         printf("\tarea = %.3lf\n", area(circle, i));
-        printf("\tintersecs:\n");
+        printf("\tintersects:\n");
         crossing_check(circle, i, number);
     }
     return 0;
--- src/props.c
+++ src/props.c
@@ -15,8 +15,8 @@ double area(figure* circle, size_t n)
 
 size_t crossing(figure* circle, size_t n, size_t i)
 {
-    if ((sqrt(pow(circle[n].x, 2) + pow(circle[n].y, 2))
-         + sqrt(pow(circle[i].x, 2) + pow(circle[i].y, 2)))
+    if ((sqrt(pow(circle[n].x - circle[i].x, 2)
+              + pow(circle[n].y - circle[i].y, 2)))
         <= (circle[n].r + circle[i].r)) {
         return 1;
     }
@@ -29,7 +29,7 @@ void crossing_check(figure* circle, size_t n, size_t number)
     for (size_t i = 0; i < number; i++) {
         if (i != n) {
             if (crossing(circle, n, i) == 1) {
-                printf("\t\t%d. circle\n", i);
+                printf("\t\t%d. circle\n", i + 1);
                 crs = 1;
             }
         }
